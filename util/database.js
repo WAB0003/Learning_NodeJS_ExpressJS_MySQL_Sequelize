2d6b3271f606a2777023f0d7f4072e6ba02989c4
Bill Brown <118410317+WAB0003@users.noreply.github.com>
2024-01-29 11:04:53 -0600

    Add files via upload

---
+++ util/database.js
@@ -0,0 +1,10 @@
+
+//BRING IN SEQUELIZE TO PROJECT
+const Sequelize = require('sequelize');
+
+// ESTABLISH NEW SEQUELIZE DB CONNECTION
+// CREATE NEW SEQUELIZE INSTANCE
+// Reference Option 3: https://sequelize.org/docs/v6/getting-started/
+// After creating an instance using MySql, type followin information:
+const sequelize = new Sequelize(db name, root directory, password, )
+module.exports = sequelize;
